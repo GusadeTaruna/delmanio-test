87af6d77564a7078842889179c84dbcae1205b11
GusadeTaruna <gusadetaruna04@gmail.com>
2023-11-23 15:42:09 +0800

    feat: fixing table removing header

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,19 +5,6 @@ import { AutoSizer } from "react-virtualized";
 import { VariableSizeGrid as Grid } from "react-window";
 
 const TableList = ({ columns, data }) => {
-  const columnWidths = columns.map(() => 150);
-  const getColumnWidth = (index) => columnWidths[index];
-
-  const { getTableProps, headerGroups, rows, prepareRow, totalColumnsWidth } =
-    useTable(
-      {
-        columns,
-        data,
-      },
-      useBlockLayout,
-      useResizeColumns
-    );
-
   const renderCell = ({ columnIndex, rowIndex, style }) => {
     const column = columns[columnIndex];
     const rowData = data[rowIndex];
@@ -37,46 +24,21 @@ const TableList = ({ columns, data }) => {
   };
 
   return (
-    <Box height="100vh">
-      <div
-        {...getTableProps()}
-        style={{ width: totalColumnsWidth, height: "100%" }}
-      >
-        <div>
-          {headerGroups.map((headerGroup) => (
-            <div {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <div
-                  key={column.id}
-                  {...column.getHeaderProps(column.getResizerProps())}
-                  style={{
-                    width: column.width,
-                    border: "1px solid gray",
-                    boxSizing: "border-box",
-                    display: "inline-block",
-                  }}
-                >
-                  {column.render("Header")}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-        <AutoSizer>
-          {({ height, width }) => (
-            <Grid
-              columnCount={columns.length}
-              columnWidth={(index) => getColumnWidth(index)}
-              height={height}
-              rowCount={data.length}
-              rowHeight={() => 35}
-              width={width}
-            >
-              {renderCell}
-            </Grid>
-          )}
-        </AutoSizer>
-      </div>
+    <Box px={4} height="70vh">
+      <AutoSizer>
+        {({ height, width }) => (
+          <Grid
+            columnCount={columns.length}
+            columnWidth={() => 150}
+            height={height}
+            rowCount={data.length}
+            rowHeight={() => 35}
+            width={width}
+          >
+            {renderCell}
+          </Grid>
+        )}
+      </AutoSizer>
     </Box>
   );
 };
